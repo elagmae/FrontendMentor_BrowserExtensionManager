30dde5c21cbe5f8d5d8f94e81cd32f65c71c7224
elagmae <lea.pouthier@gmail.com>
2026-09-08 14:08:12 +0200

    fixed automatic dark mode

--- js/theme-toggle-handler.js
+++ js/theme-toggle-handler.js
@@ -4,21 +4,22 @@ UpdateThemePreference();
 toggleThemeButton.onclick = ToggleThemePreference;
 
 function UpdateThemePreference()
-{ 
-    if(localStorage.getItem("theme") === "dark-mode")
-    {
-        document.body.classList.add("dark-mode");
-        document.body.classList.remove("light-mode");
+{
 
-        toggleThemeButton.innerHTML = `<img src="./assets/images/icon-sun.svg" alt="Light mode icon">`;
-    }
-
-    else
+    if(localStorage.getItem("theme") === "light-mode")
     {
         document.body.classList.add("light-mode");
         document.body.classList.remove("dark-mode");
 
         toggleThemeButton.innerHTML = `<img src="./assets/images/icon-moon.svg" alt="Dark mode icon">`;
+    }
+
+    else
+    {
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+
+        toggleThemeButton.innerHTML = `<img src="./assets/images/icon-sun.svg" alt="Light mode icon">`;
     }
 }
 
